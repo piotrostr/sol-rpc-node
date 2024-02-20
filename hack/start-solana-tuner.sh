80ff5e5c42a062a77ef147e836f2503cad9d5491
Piotr Ostrowski <piotrostr@google.com>
2024-02-20 20:30:28 +0100

    add script for setting up the tuner (required)

---
+++ hack/start-solana-tuner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo bash -c "cat >/etc/sysctl.d/21-solana-validator.conf <<EOF
+# Increase UDP buffer sizes
+net.core.rmem_default = 134217728
+net.core.rmem_max = 134217728
+net.core.wmem_default = 134217728
+net.core.wmem_max = 134217728
+
+# Increase memory mapped files limit
+vm.max_map_count = 1000000
+
+# Increase number of allowed open file descriptors
+fs.nr_open = 1000000
+EOF"
+
+sudo sysctl -p /etc/sysctl.d/21-solana-validator.conf
