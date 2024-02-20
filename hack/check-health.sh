b93cd7afcc9629e09f12609689ff439621fb9c73
Piotr Ostrowski <piotrostr@google.com>
2024-02-20 23:14:49 +0100

    include block height method

--- hack/check-health.sh
+++ hack/check-health.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+export IP_ADDRESS=localhost
+export PORT=8899
+
 curl -X POST \
   -H "Content-Type: application/json" \
   -d '{"jsonrpc":"2.0","id":1, "method":"getHealth"}' \
-  http://localhost:8899
+  http://$IP_ADDRESS:$PORT
+
+curl -X POST \
+  -d '{ "jsonrpc": "2.0", "id": 1, "method": "getBlockHeight", "params": []}' \
+  -H "Content-Type: application/json" \
+  http://$IP_ADDRESS:$PORT 
